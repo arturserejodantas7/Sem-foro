ceb673049acfe30a14429763c95dad660f4b7eff
arturserejodantas7 <arturserejodantas7@usp.br>
2026-03-20 11:00:26 -0300

    Semáforo.ino
    Atividade Semáforo de Trânsito

--- blink.ino
+++ blink.ino
@@ -23,18 +23,30 @@
 */
 
 // the setup function runs once when you press reset or power the board
+// Semáforo com 3 LEDs
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // Verde
+  pinMode(8, OUTPUT);           // Vermelho
+  pinMode(7, OUTPUT);           // Amarelo
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // VERDE ligado
+  digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(8, LOW);
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(7, LOW);
+  delay(3000); // 3 segundos
+
+  // AMARELO ligado
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(7, HIGH);
+  delay(2000); // 2 segundos
+
+  // VERMELHO ligado
+  digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(8, HIGH);
-  delay(1000);                       // wait for a second
+  digitalWrite(7, LOW);
+  delay(3000); // 3 segundos
 }
